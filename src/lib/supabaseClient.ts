7eb1dccc2b3f704dfcc3e10485a72e01e904c7dc
R3S3T79 <155052638+R3S3T79@users.noreply.github.com>
2025-07-12 22:55:13 +0200

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,17 +1,13 @@
 // src/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL!
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY!
 
-if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-  throw new Error("❌ Supabase URL o ANON KEY mancante. Controlla il file .env o l'importazione delle variabili.");
-}
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
     persistSession: true,
     detectSessionInUrl: true,
     autoRefreshToken: true,
   },
-});
+})
