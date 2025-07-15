629169c35e3eb36377573e1af116c131df991e0f
R3S3T79 <155052638+R3S3T79@users.noreply.github.com>
2025-07-15 21:48:20 +0200

    Create index.ts

---
+++ src/pages/index.ts
@@ -0,0 +1,18 @@
+// src/pages/index.ts
+
+export { default as Home } from './Home';
+export { default as Calendario } from './Calendario';
+export { default as Risultati } from './Risultati';
+export { default as RosaGiocatori } from './RosaGiocatori';
+export { default as ListaSquadre } from './ListaSquadre';
+export { default as StatisticheSquadra } from './StatisticheSquadra';
+export { default as StatisticheGiocatori } from './StatisticheGiocatori';
+export { default as ProssimaPartita } from './ProssimaPartita';
+export { default as Tornei } from './tornei/Tornei';
+export { default as AdminDashboard } from './AdminDashboard';
+export { default as AdminPanel } from './AdminPanel';
+
+export { default as LoginPage } from './LoginPage';
+export { default as RegisterPage } from './RegisterPage';
+export { default as ConfirmPage } from './ConfirmPage';
+export { default as AuthCallback } from './AuthCallback';
