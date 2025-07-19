5bec2020ce7fda7cee90a06d87e63f895656762f
R3S3T79 <155052638+R3S3T79@users.noreply.github.com>
2025-07-19 10:30:58 +0200

    Update roles.ts

--- src/lib/roles.ts
+++ src/lib/roles.ts
@@ -1,8 +1,7 @@
 // src/lib/roles.ts
-
 export enum UserRole {
-  Authenticated = 'authenticated',
-  User          = 'user',
-  Creator       = 'creator',
-  Admin         = 'admin',
+  Authenticated = "authenticated",
+  User          = "user",
+  Creator       = "creator",
+  Admin         = "admin",
 }
