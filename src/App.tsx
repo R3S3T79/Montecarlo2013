7343bf1debb711d38c7e6f197518cac96fd50700
R3S3T79 <155052638+R3S3T79@users.noreply.github.com>
2025-07-20 12:31:16 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import RegisterPage    from "./pages/RegisterPage";
 import ConfirmPage     from "./pages/ConfirmPage";
 import AuthCallback    from "./pages/AuthCallback";
 
-// pagine per ogni utente autenticato
+// pagine per ogni utente autenticato (ora NESTATE dentro SidebarLayout)
 import RosaGiocatori      from "./pages/RosaGiocatori";
 import DettaglioGiocatore from "./pages/DettaglioGiocatore";
 import AggiungiGiocatore  from "./pages/AggiungiGiocatore";
@@ -40,7 +40,6 @@ import Step5_Eliminazione  from "./pages/tornei/NuovoTorneo/Step5_Eliminazione";
 import Step6_Eliminazione  from "./pages/tornei/NuovoTorneo/Step6_Eliminazione";
 import Step6_FaseGironi    from "./pages/tornei/NuovoTorneo/Step6_FaseGironi";
 import Step6_GironeUnico   from "./pages/tornei/NuovoTorneo/Step6_GironeUnico";
-// ** qui ho cambiato **
 import Step7_FaseGironi    from "./pages/tornei/NuovoTorneo/Step7_FaseGironi";
 
 // admin only
@@ -51,18 +50,13 @@ export default function App() {
   return (
     <AuthProvider>
       <Routes>
-        {/* pubbliche */}
+        {/* ROTTE PUBBLICHE */}
         <Route path="/login"         element={<LoginPage />} />
         <Route path="/register"      element={<RegisterPage />} />
         <Route path="/confirm"       element={<ConfirmPage />} />
         <Route path="/auth/callback" element={<AuthCallback />} />
 
-        {/* utenti autenticati */}
-        <Route path="/rosa"               element={<RosaGiocatori />} />
-        <Route path="/giocatore/:id"      element={<DettaglioGiocatore />} />
-        <Route path="/aggiungi-giocatore" element={<AggiungiGiocatore />} />
-
-        {/* protette nella sidebar */}
+        {/* TUTTE LE ROTTE PROTETTE CON SIDEBAR */}
         <Route
           path="/*"
           element={
@@ -71,59 +65,59 @@ export default function App() {
             </ProtectedRoute>
           }
         >
+          {/* Home */}
           <Route index element={<Home />} />
 
-          <Route path="calendario"       element={<Calendario />} />
-          <Route path="pre-partita/:id"  element={<DettaglioPrePartita />} />
+          {/* Rosa Giocatori e dettagli */}
+          <Route path="rosa"               element={<RosaGiocatori />} />
+          <Route path="giocatore/:id"      element={<DettaglioGiocatore />} />
+          <Route path="aggiungi-giocatore" element={<AggiungiGiocatore />} />
 
+          {/* Calendario */}
+          <Route path="calendario"      element={<Calendario />} />
+          <Route path="pre-partita/:id" element={<DettaglioPrePartita />} />
+
+          {/* Risultati */}
           <Route path="risultati"        element={<Risultati />} />
           <Route path="nuova-partita"    element={<NuovaPartitaPage />} />
           <Route path="partita/:id"      element={<DettaglioPartita />} />
           <Route path="partita/:id/edit" element={<EditPartitaPage />} />
 
+          {/* Squadre */}
           <Route path="squadre"          element={<ListaSquadre />} />
           <Route path="squadre/nuova"    element={<NuovaSquadra />} />
           <Route path="squadre/:id"      element={<DettaglioSquadra />} />
           <Route path="squadre/:id/edit" element={<EditSquadra />} />
 
+          {/* Statistiche */}
           <Route path="statistiche/squadra"   element={<StatisticheSquadra />} />
           <Route path="statistiche/giocatori" element={<StatisticheGiocatori />} />
           <Route path="prossima-partita"       element={<ProssimaPartita />} />
 
-          {/* elenco tornei */}
+          {/* Tornei */}
           <Route path="tornei" element={<Tornei />} />
-
-          {/* STEP 5: eliminazione diretta */}
           <Route
             path="tornei/nuovo/step5-eliminazione/:torneoId"
             element={<Step5_Eliminazione />}
           />
-
-          {/* STEP 6a: tabellone KO */}
           <Route
             path="tornei/nuovo/step6-eliminazione/:torneoId"
             element={<Step6_Eliminazione />}
           />
-
-          {/* STEP 6b: fase a gironi */}
           <Route
             path="tornei/nuovo/step6-fasegironi/:torneoId"
             element={<Step6_FaseGironi />}
           />
-
-          {/* STEP 6c: girone unico */}
           <Route
             path="tornei/nuovo/step6-gironeunico/:torneoId"
             element={<Step6_GironeUnico />}
           />
-
-          {/* STEP 7: gironi finali */}
           <Route
             path="tornei/nuovo/step7-fasegironi/:torneoId"
             element={<Step7_FaseGironi />}
           />
 
-          {/* creator/admin */}
+          {/* Admin only */}
           <Route path="admin"       element={<AdminDashboard />} />
           <Route path="admin-panel" element={<AdminPanel     />} />
         </Route>
